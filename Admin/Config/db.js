f0e73320538699bafdec7382401a5a031a23054e
Sammyak555 <sammyak.deosale.1@gmail.com>
2023-03-25 15:53:41 +0530

    aad

--- Admin/Config/db.js
+++ Admin/Config/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const connection = mongoose.connect(process.env.mongoURL)
-mongoose.set('strictQuery', false);
 module.exports={
     connection
 }
