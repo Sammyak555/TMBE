e3dc2762a061e8f5d6de7b5fe60a05bf5b545c73
Sammyak555 <sammyak.deosale.1@gmail.com>
2023-03-25 16:16:23 +0530

    ch

--- Admin/Config/db.js
+++ Admin/Config/db.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 mongoose.set('strictQuery', false);
-const connection = mongoose.connect(process.env.mongoURL)
+const connection = mongoose.connect(process.env.mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB', err);
+});
 module.exports={
     connection
 }
